cd9ef7b8a55f2f0d980573954eb19b59e9159103
NicolasK009 <156432341+NicolasK009@users.noreply.github.com>
2024-01-12 13:24:00 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Código generado automáticamente. No editar.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Dapr JR":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . e e e e e . . . . . . 
+. . . . . e e e e e . . . . . . 
+. . . . . d d d d e . . . . . . 
+. . . . . f d f d d . . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . . . f f f d d . . . . . . 
+. . . . . . d d . . . . . . . . 
+. . . . . . d d . . . . . . . . 
+. . . 8 8 8 8 8 8 8 8 8 . . . . 
+. . . 8 . 8 8 8 8 8 . 8 . . . . 
+. . . d . 8 8 8 8 8 . d . . . . 
+. . . d . 8 8 8 8 8 . d . . . . 
+. . . . . 8 8 . 8 8 . . . . . . 
+. . . . . 7 7 . 7 7 . . . . . . 
+. . . . . 7 7 . 7 7 . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
